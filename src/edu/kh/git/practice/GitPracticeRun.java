a9ee2d45be5a40d290365e6d41d3db53572edf04
SeulKiLee <seulki901234@gmail.com>
2023-02-20 19:20:15 +0900

    커밋2번째(추가+삭제)

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -5,8 +5,9 @@ public class GitPracticeRun {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("안녕하세요");
+
 		System.out.println("git연습용 파일입니다");
+		System.out.println("git연결");
 	}
 
 }

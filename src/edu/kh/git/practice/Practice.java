c8ec2d6558e38db63b778b56a5376fd38f731391
SeulKiLee <seulki901234@gmail.com>
2023-02-20 19:25:44 +0900

    처음커밋

---
+++ src/edu/kh/git/practice/Practice.java
@@ -0,0 +1,12 @@
+package edu.kh.git.practice;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("연습용");
+System.out.println("1");
+System.out.println("2");
+	}
+
+}
